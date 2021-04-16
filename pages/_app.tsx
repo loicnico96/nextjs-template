e7801b6cd39a4167f283fd84f484692c1e382e80
loicnico96 <loicnico96@gmail.com>
2021-04-16 17:14:44 +0200

    Remove default content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,17 @@ import { AppProps } from "next/app"
 import React from "react"
 
 import Layout from "components/Layout"
+
 import "styles/globals.css"
+import Head from "next/head"
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <title>App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
