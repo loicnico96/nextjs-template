f3fa4dd701adb281c05358dda419726eea69470e
loicnico96 <loicnico96@gmail.com>
2021-04-16 13:18:51 +0200

    Setup Typescript

---
+++ components/Layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react"
+
+export type LayoutProps = {
+  children: ReactNode
+}
+
+function Layout({ children }: LayoutProps) {
+  return (
+    <div className="container">
+      {children}
+    </div>
+  )
+}
+
+export default Layout
