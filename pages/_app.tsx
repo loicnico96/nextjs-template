23b4681729b8f4dca6bb60d45a4ca13269e80fea
loicnico96 <loicnico96@gmail.com>
2021-04-16 15:05:52 +0200

    Setup ESLint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
+import { AppProps } from "next/app"
+import React from "react"
+
 import Layout from "components/Layout"
 import "styles/globals.css"
 
-function App({ Component, pageProps }) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head"
+import React from "react"
+
 import styles from "../styles/Home.module.css"
 
 export default function Home() {
