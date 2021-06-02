37d2caff8989a2c6022ff23200b8bb5e744efa85
loicnico96 <loicnico96@gmail.com>
2021-06-02 09:58:32 +0200

    Cleanup Next.js config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,9 @@
-const path = require("path");
-
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
 module.exports = withBundleAnalyzer({
-  webpack(config, { dev }) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      api: path.resolve(__dirname, "api"),
-      components: path.resolve(__dirname, "components"),
-      hooks: path.resolve(__dirname, "hooks"),
-      lib: path.resolve(__dirname, "lib"),
-      pages: path.resolve(__dirname, "pages"),
-      styles: path.resolve(__dirname, "styles"),
-    };
-
-    return config;
-  },
+  future: { webpack5: true },
+  poweredByHeader: false,
+  reactStrictMode: true
 });
